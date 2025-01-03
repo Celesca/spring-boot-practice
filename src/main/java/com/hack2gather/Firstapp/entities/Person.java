3cbf33215bbbeda90bf519b9beab57c31cf9261f
celesca <folk.sawit@gmail.com>
2025-01-03 12:57:00 +0700

    getter setter

--- src/main/java/com/hack2gather/Firstapp/entities/Person.java
+++ src/main/java/com/hack2gather/Firstapp/entities/Person.java
@@ -17,10 +17,46 @@ public class Person {
     @Column(name = "lastname")
     private String lname;
 
+    public void Persona() {
+
+    }
+
     public Person(String fname, String lname) {
         this.fname = fname;
         this.lname = lname;
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    //
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", fname='" + fname + '\'' +
+                ", lname='" + lname + '\'' +
+                '}';
+    }
 }
