6338ad6ea1f081d37333828185600b67a7cbcbc8
celesca <folk.sawit@gmail.com>
2025-01-03 13:31:10 +0700

    add: personDAO

---
+++ src/main/java/com/hack2gather/Firstapp/repository/PersonDAO.java
@@ -0,0 +1,7 @@
+package com.hack2gather.Firstapp.repository;
+
+import com.hack2gather.Firstapp.entities.Person;
+
+public interface PersonDAO {
+    void save(Person person);
+}
---
+++ src/main/java/com/hack2gather/Firstapp/repository/PersonRepository.java
@@ -0,0 +1,21 @@
+package com.hack2gather.Firstapp.repository;
+
+import com.hack2gather.Firstapp.entities.Person;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PersonRepository implements PersonDAO{
+    private EntityManager entityManager;
+
+    @Autowired
+    public PersonRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public void save(Person person) {
+        entityManager.persist(person);
+    }
+}
