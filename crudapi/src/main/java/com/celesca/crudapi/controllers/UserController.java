9ee47a2ffdfb72354c8ef6b4060b0e7477cebf4a
celesca <folk.sawit@gmail.com>
2025-01-03 16:28:49 +0700

    add: user repository

--- crudapi/src/main/java/com/celesca/crudapi/controllers/UserController.java
+++ crudapi/src/main/java/com/celesca/crudapi/controllers/UserController.java
@@ -1,27 +1,24 @@
 package com.celesca.crudapi.controllers;
 
+import com.celesca.crudapi.entity.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
 
-    @GetMapping("/users")
-    public String getUsers() {
-        return "All users";
-    }
-
-    @GetMapping("/about")
-    public String getAbout() {
-        return "เกี่ยวกับเรา";
-    }
-
-    @GetMapping("/contact")
-    public String getContact() {
-        return "ติดต่อเรา";
+    @GetMapping("/user")
+    public List<User> getUsers() {
+        List<User> data = new ArrayList<User>();
+        data.add(new User("John", "Doe"));
+        data.add(new User("Jane", "Doe"));
+        return data;
     }
 
 }
