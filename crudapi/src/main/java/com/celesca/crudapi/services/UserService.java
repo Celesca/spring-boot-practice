43dc00f176e4d2204ff9332289936e6f10ac2850
celesca <folk.sawit@gmail.com>
2025-01-19 00:57:02 +0700

    add: userService save

---
+++ crudapi/src/main/java/com/celesca/crudapi/services/UserService.java
@@ -0,0 +1,7 @@
+package com.celesca.crudapi.services;
+
+import com.celesca.crudapi.entity.User;
+
+public interface UserService {
+    User save(User user);
+}
---
+++ crudapi/src/main/java/com/celesca/crudapi/services/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.celesca.crudapi.services;
+
+import com.celesca.crudapi.entity.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+    @Override
+    public User save(User user) {
+        return null;
+    }
+}
