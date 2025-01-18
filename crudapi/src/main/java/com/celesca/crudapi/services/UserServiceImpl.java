507f46b47f95110f6e9ac40601ae4bcd60eb60cd
celesca <folk.sawit@gmail.com>
2025-01-19 00:58:10 +0700

    dependency injection

--- crudapi/src/main/java/com/celesca/crudapi/services/UserServiceImpl.java
+++ crudapi/src/main/java/com/celesca/crudapi/services/UserServiceImpl.java
@@ -1,10 +1,19 @@
 package com.celesca.crudapi.services;
 
 import com.celesca.crudapi.entity.User;
+import com.celesca.crudapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService{
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
     @Override
     public User save(User user) {
         return null;
