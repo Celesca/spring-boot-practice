3541651ea592864a36f6d1db524e367a371f9456
celesca <folk.sawit@gmail.com>
2025-01-19 11:34:32 +0700

    implement findById

--- crudapi/src/main/java/com/celesca/crudapi/services/UserService.java
+++ crudapi/src/main/java/com/celesca/crudapi/services/UserService.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface UserService {
     User save(User user);
     List<User> findAll();
+    User findById(Integer id);
 }
--- crudapi/src/main/java/com/celesca/crudapi/services/UserServiceImpl.java
+++ crudapi/src/main/java/com/celesca/crudapi/services/UserServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -24,5 +25,18 @@ public class UserServiceImpl implements UserService{
     @Override
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    @Override
+    public User findById(Integer id)  {
+        Optional<User> result = userRepository.findById(id);
+        User data = null;
+        if (result.isPresent()) {
+            data = result.get();
+        }
+        else {
+            throw new RuntimeException("User not found");
+        }
+        return data;
     }
 }
